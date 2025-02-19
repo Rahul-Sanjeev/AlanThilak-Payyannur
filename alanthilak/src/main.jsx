2bd482e50491667c665e19714a5290d83945a5eb
Rahul Sanjeev <rahulsanjeev9@gmail.com>
2025-02-19 20:50:15 +0530

    import link updated

--- alanthilak/src/main.jsx
+++ alanthilak/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
