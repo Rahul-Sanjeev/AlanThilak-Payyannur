c3b0afe77d76787ddb1a7c5222cb714d958fc59a
Rahul Sanjeev <rahulsanjeev9@gmail.com>
2025-09-30 01:56:47 +0530

    AudioGate Disabled

--- alanthilak/src/App.jsx
+++ alanthilak/src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import AudioGate from './components/AudioGate/AudioGate';
+// import { useState } from 'react';
+// import AudioGate from './components/AudioGate/AudioGate';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Services from './components/Services/Services';
@@ -10,34 +10,34 @@ import Footer from './components/Footer/Footer';
 import Branches from './components/Branches/Branches';
 import Leadership from './components/Leadership/Leadership';
 import Gallery from './components/Gallery/Gallery';
-import AudioFile from './assets/Audio/ATKSI-Audio.mp3'; // Import the audio file directly
+// import AudioFile from './assets/Audio/ATKSI-Audio.mp3'; // Import the audio file directly
 
 const App = () => {
-    const [audioAllowed, setAudioAllowed] = useState(false);
-    const [audioElement, setAudioElement] = useState(null);
+    //     const [audioAllowed, setAudioAllowed] = useState(false);
+    //     const [audioElement, setAudioElement] = useState(null);
 
-    const handleUnlock = () => {
-        const audio = new Audio(AudioFile); // Create a single audio instance
-        audio.loop = true;
+    // const handleUnlock = () => {
+    //     const audio = new Audio(AudioFile); // Create a single audio instance
+    //     audio.loop = true;
 
-        audio.play()
-            .then(() => {
-                setAudioAllowed(true);
-                setAudioElement(audio);
-            })
-            .catch(error => {
-                console.error('Audio play failed:', error);
-                setAudioAllowed(true);
-            });
-    };
+    //     audio.play()
+    //         .then(() => {
+    //             setAudioAllowed(true);
+    //             setAudioElement(audio);
+    //         })
+    //         .catch(error => {
+    //             console.error('Audio play failed:', error);
+    //             setAudioAllowed(true);
+    //         });
+    // };
 
     return (
         <div>
-            {!audioAllowed ? (
-                <AudioGate onUnlock={handleUnlock} />
+            (
+            {/* <AudioGate onUnlock={handleUnlock} /> */}
             ) : (
                 <>
-                    <Navbar audioElement={audioElement} />
+                <Navbar />
                     <Hero />
                     <About />
                     <Leadership />
@@ -48,7 +48,7 @@ const App = () => {
                         <Socials />
                         <Footer />
                 </>
-            )}
+            )
         </div>
     );
 };
